7675e4945e7e8c84eb65bf9babcbb2c709aa6373
thuyduong7 <duongbui0712@gmail.com>
2022-02-17 16:16:15 +0700

    update

---
+++ CelebCrawler/CelebCrawler/pipelines.py
@@ -0,0 +1,52 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+from scrapy import settings
+import logging
+import pymongo
+
+
+class MongoPipeline:
+    collection_name = 'celebs'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # pull in information from settings.py
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        # initializing spider
+        # opening db connection
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        # clean up when spider is closed
+        self.client.close()
+
+    def process_item(self, item, spider):
+        # how to handle each post
+        # self.db[self.collection_name].insert_one(dict(item))
+        item_as_dict = dict(item)
+        item_id = item_as_dict['_id']
+
+        self.db[self.collection_name].replace_one(
+            filter={'_id': item_id},
+            replacement=item_as_dict,
+            upsert=True
+        )
+
+        logging.debug(f'Post added/updated to MongoDB')
+        return item
